b065ecd47d7af3e36d581a7838b76f59ddaabca5
Hai Nguyen <hai.nguyen@neo-lab.co.jp>
2021-03-13 16:31:24 +0900

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  "title": "Hải dz",
+  "description": "viết nhố nhăng tý",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.png"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "locales": {
+    "/": {
+      "lang": 'en-US',
+    },
+  },
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "test",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "#"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "Haidz",
+    "authorAvatar": "/avatar.png",
+    "startYear": "2017"
+  },
+  "markdown": {
+    "lineNumbers": true
+  },
+  plugins: [
+    ['@vuepress-reco/vuepress-plugin-rss', {
+      'site_url': 'https://www.facebook.com/hainx.jp/'
+    }],
+    ['@vuepress-reco/vuepress-plugin-kan-ban-niang', {
+      'theme': ['z16'],
+      'clean': true
+    }]
+  ]
+}
---
+++ blogs/other/guide.md
@@ -0,0 +1,26 @@
+---
+title: vuepress-theme-reco
+date: 2019-04-09
+---
+
+## Use
+
+**Build**
+
+```bash
+npm run build
+
+# or
+
+yarn build
+```
+
+**Server**
+
+```bash
+npm run dev
+
+# or
+
+yarn dev
+```
